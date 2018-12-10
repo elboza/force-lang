967211b0efd5bb4d182b14ebc2d2fd7564dade1d
Fernando Iazeolla <fernando.iazeolla@gmail.com>
2018-12-11 00:41:32 +0100

    added if then (else)

--- eval.js
+++ eval.js
@@ -32,6 +32,35 @@ class Eval{
 		str += 'at ' + where.line +','+ where.col;
 		return str;
 	}
+	eval_if(stream, list){
+		//log.info('if.:.');
+		var y;
+		var body = [];
+		var else_body = [];
+		var then_body = [];
+		var level=0;
+		if(list){
+			//var list_copy=JSON.parse(JSON.stringify(list));
+			var list_copy=list;
+			while(y=list_copy.shift()){
+				if(y._datum=='if') {level++;}
+				if(y._datum=='then') { if(level==0){then_body = body; break;}else{level--;}}
+				if(y._datum=='else') { if(level==0) {else_body = body; body = []; continue;}}
+				body.push(y);
+			}
+		} else {
+			while((y=read.read(stream))!=false){
+				if(y._datum=='if') {level++;}
+				if(y._datum=='then') { if(level==0){then_body = body; break;}else{level--;}}
+				if(y._datum=='else') { if(level==0) {else_body = body; body = []; continue;}}
+				body.push(y);
+			}
+		}
+		//log.info({else_body});
+		//log.info({then_body});
+		this.eval_parsed(then_body);
+		return list_copy;
+	}
 	eval_parsed_step(e){
 		var y;
 		if(this.mode == 'interpret'){
@@ -70,7 +99,15 @@ class Eval{
 		}
 	}
 	eval_parsed(e){
-		for(var item of e){
+		var item;
+		var list_copy=JSON.parse(JSON.stringify(e));
+		//for(var item of e){
+		while(item=list_copy.shift()){
+			if(item._datum == 'if'){
+					log.info('if.:.');
+					list_copy=this.eval_if(null,list_copy);
+					continue;
+				}
 			if(item._datum == ';' || item._datum == 'exit') break;
 			this.eval_parsed_step(item);
 		}
@@ -101,6 +138,10 @@ class Eval{
 				if(x._datum == 'see'){
 					var func_name = read.read(stream);
 					NativeLib.see_func(func_name._datum);
+					continue;
+				}
+				if(x._datum == 'if'){
+					this.eval_if(stream);
 					continue;
 				}
 			}
